4d044edfa6059ed8f65534d3319b5427c10e8406
Manu Pathak <83893814+manu367@users.noreply.github.com>
2025-07-02 22:37:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const value=document.getElementById("value");
+const button=document.getElementById("button");
+let value1=0;
+button.addEventListener("click",()=>{
+            value1=value1+1;
+	value.innerText=`value is ${value1}`;
+        });
